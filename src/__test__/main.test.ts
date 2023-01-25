c02d4fc05c9a2258442125d56bd3c6dadf1a3c56
emiliamassing <emiliamassings@gmail.com>
2023-01-25 19:52:25 +0100

    changes to test for displayError

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,12 +1,15 @@
-/*** @jest-environment jsdom*/
+/*** 
+ * @jest-environment jsdom
+*/
+
 import { displayError } from "../ts/main";
 
 //Test for displaying error - "displayError"
 test('Should display error', () => {
-    const error = 'error';
-    const errorDiv: HTMLDivElement = (document.getElementById(error) as HTMLDivElement);
-
+    const error = document.body.innerHTML = `<div id="error" class="error"></div>`;
+    const errorDiv: HTMLDivElement = (document.getElementById('error') as HTMLDivElement); 
+    
     errorDiv.innerHTML = error;
 
-    expect(errorDiv.innerHTML).toBe('error');
+    expect(errorDiv.innerHTML).toBe(error);
 });
