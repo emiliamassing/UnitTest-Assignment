f93bddcd7ff16c787993d57b54bf0af48750f49f
emiliamassing <emiliamassings@gmail.com>
2023-01-25 20:29:21 +0100

    added describe() for displayError()

--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -4,12 +4,21 @@
 
 import { displayError } from "../ts/main";
 
-//Test for displaying error - "displayError"
-test('Should display error', () => {
-    const error = document.body.innerHTML = `<div id="error" class="error"></div>`;
-    const errorDiv: HTMLDivElement = (document.getElementById('error') as HTMLDivElement); 
-    
-    errorDiv.innerHTML = error;
 
-    expect(errorDiv.innerHTML).toBe(error);
+describe('Should add/remove css class depending on argument value - displayError()', () => {
+    
+    //Test for reading innerHTML - "displayError"
+    test('Should display read innerHTML', () => {
+        const error = document.body.innerHTML = `<div id="error" class="error"></div>`;
+        const errorDiv: HTMLDivElement = (document.getElementById('error') as HTMLDivElement); 
+        
+        errorDiv.innerHTML = error;
+
+        expect(errorDiv.innerHTML).toBe(error);
+    });
+
+    //Test for if if-statement is true - "displayError"
+    test('Should add error class', () => {
+
+    });
 });
