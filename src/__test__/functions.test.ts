cd3093bcc5e24a6ba9862fa2a92a2c64eaa3ff98
emiliamassing <emiliamassings@gmail.com>
2023-01-26 10:26:06 +0100

    added describe for functions.test.ts

--- src/__test__/functions.test.ts
+++ src/__test__/functions.test.ts
@@ -1,8 +1,8 @@
 import { addTodo, changeTodo, removeAllTodos } from "../ts/functions";
 import { Todo } from "../ts/models/Todo";
 
-
 //Se till även testa så funktionen som gör att du kan avmarkera todo-uppgiften funkar
+
 //Test for adding todo - "addTodo"
 /*test('Should add todo', () => {
 
@@ -12,26 +12,27 @@ import { Todo } from "../ts/models/Todo";
 
 });*/
 
+describe('Tests for all functions in functions.ts', () => {
+    //Test for completeing todo - "changeTodo"
+    test('Should switch from false to true', () => {
 
-//Test for completeing todo - "changeTodo"
-test('Should switch from false to true', () => {
+        const todo = new Todo('To do', false);
 
-    const todo = new Todo('To do', false);
+        changeTodo(todo);
 
-    changeTodo(todo);
+        expect(todo.done).toBe(true);
+    });
 
-    expect(todo.done).toBe(true);
-});
+    //Test for clearing all todos - "removeAllTodos"
+    test('Should remove all todos', () => {
 
-//Test for clearing all todos - "removeAllTodos"
-test('Should remove all todos', () => {
+        let todoList: Todo[] = [
+            {text: "Todo 1", done: false}, // Shows that both done and undone todos gets removed
+            {text: "Todo 2", done: true}
+        ];
 
-    let todoList: Todo[] = [
-        {text: "Todo 1", done: false}, // Shows that both done and undone todos gets removed
-        {text: "Todo 2", done: true}
-    ];
+        removeAllTodos(todoList);
 
-    removeAllTodos(todoList);
-
-    expect(todoList.length).toBe(0);
+        expect(todoList.length).toBe(0);
+    });
 });
--- src/__test__/main.test.ts
+++ src/__test__/main.test.ts
@@ -1,24 +1,33 @@
 /*** 
- * @jest-environment jsdom
+* @jest-environment jsdom
 */
 
 import { displayError } from "../ts/main";
 
+beforeEach(() => {
+    document.body.innerHTML = '';
+});
+
 
 describe('Should add/remove css class depending on argument value - displayError()', () => {
-    
-    //Test for reading innerHTML - "displayError"
-    test('Should display read innerHTML', () => {
-        const error = document.body.innerHTML = `<div id="error" class="error"></div>`;
-        const errorDiv: HTMLDivElement = (document.getElementById('error') as HTMLDivElement); 
-        
-        errorDiv.innerHTML = error;
 
-        expect(errorDiv.innerHTML).toBe(error);
+    //Test for reading innerHTML - "displayError"
+    test('Should display innerHTML', () => {
+        document.body.innerHTML = `
+            <div id="error" class="error"></div>
+        `;
+        let errorText = 'error msg';
+       
+        
+        displayError(errorText, true);
+
+        let errorDiv = (document.getElementById('error') as HTMLDivElement);
+        expect(errorDiv).not.toBeNull();
+        expect(errorDiv.innerHTML).toBe(errorText);
     });
 
     //Test for if if-statement is true - "displayError"
-    test('Should add error class', () => {
+    /*test('Should add error class', () => {
 
-    });
+    });*/
 });
