040c6decc748a22fc3cfb9c52c1b93582be8f9ce
emiliamassing <emiliamassings@gmail.com>
2023-01-27 10:58:44 +0100

    updated coverage

--- src/__test__/functions.test.ts
+++ src/__test__/functions.test.ts
@@ -1,38 +1,52 @@
-import { addTodo, changeTodo, removeAllTodos } from "../ts/functions";
+import * as functions from "../ts/functions";
+
 import { Todo } from "../ts/models/Todo";
 
 //Se till även testa så funktionen som gör att du kan avmarkera todo-uppgiften funkar
 
-//Test for adding todo - "addTodo"
-/*test('Should add todo', () => {
+describe('Tests for addTodo()', () => {
+    //Test for adding todo - "addTodo()"
+    test('Should add new todo, if', () => {
+        //Arrange
+        let todo: Todo[] = [];
+        const testText = 'Dressage with Merlin';
+        const length = todo.length; 
+        
+        //Act
+        functions.addTodo(testText, todo);
 
-    const shortText = new Todo('no', false);
-    
-    
-
-});*/
-
-describe('Tests for all functions in functions.ts', () => {
-    //Test for completeing todo - "changeTodo"
-    test('Should switch from false to true', () => {
-
-        const todo = new Todo('To do', false);
-
-        changeTodo(todo);
-
-        expect(todo.done).toBe(true);
+        //Assert
+        expect(todo.length).toBe(length+1);
+        expect(todo[todo.length-1].text).toBe('Dressage with Merlin');
     });
 
-    //Test for clearing all todos - "removeAllTodos"
-    test('Should remove all todos', () => {
-
-        let todoList: Todo[] = [
-            {text: "Todo 1", done: false}, // Shows that both done and undone todos gets removed
-            {text: "Todo 2", done: true}
-        ];
-
-        removeAllTodos(todoList);
-
-        expect(todoList.length).toBe(0);
+    //Test for trying to add too short text - "addTodo()"
+    test('Should not add new todo, else', () => {
+        let todo: Todo[] = [];
+        const testText = '';
+        const length = todo.length; 
     });
+});
+
+//Test for completeing todo - "changeTodo"
+test('Should switch from false to true', () => {
+
+    const todo = new Todo('Brush Aramis', false);
+
+    functions.changeTodo(todo);
+
+    expect(todo.done).toBe(true);
+});
+
+//Test for clearing all todos - "removeAllTodos"
+test('Should remove all todos', () => {
+
+    let todoList: Todo[] = [
+        {text: 'Dressage with Merlin', done: false}, // Shows that both done and undone todos gets removed
+        {text: 'Brush Aramis', done: true}
+    ];
+
+    functions.removeAllTodos(todoList);
+
+    expect(todoList.length).toBe(0);
 });
